5630f6d114794b3204c4ad8fd6bd999edf1418ac
mehedishishir0 <mehedihasanshishir@gmail.com>
2025-08-20 10:58:12 +0600

    about page and ui fixup

---
+++ src/components/aboutHero.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AboutHero = () => {
+  return (
+    <div className='bg-[#4B1E2F] w-full h-[380px]'>
+        <div className='flex flex-col justify-center items-center gap-6 h-full '>
+            <h1 className='text-5xl font-bold text-[#FFFFFF]'>About GUEHI AND CO</h1>
+            <p className='text-[#FBFAF8] text-[18px] font-normal'>Transforming the lending industry with transparency, technology, and trust</p>
+        </div>
+    </div>
+  )
+}
+
+export default AboutHero
